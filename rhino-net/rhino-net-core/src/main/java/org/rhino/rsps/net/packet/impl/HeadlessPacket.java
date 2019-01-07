962506500833b2868ff675c4731eacc0e3ad5019
Unknown <ha.nnesje.lowette@gmail.com>
2019-01-07 11:32:35 +0100

    headless packet btw

--- rhino-net/rhino-net-core/src/main/java/org/rhino/rsps/net/packet/impl/HeadlessPacket.java
+++ rhino-net/rhino-net-core/src/main/java/org/rhino/rsps/net/packet/impl/HeadlessPacket.java
@@ -1,4 +1,35 @@
 package org.rhino.rsps.net.packet.impl;
 
-public class VariableSizePacket {
+import org.rhino.rsps.net.packet.Packet;
+import org.rhino.rsps.net.packet.PacketIdentifier;
+import org.rhino.rsps.net.stream.InputStream;
+
+/**
+ * A packet that contains only a payload
+ */
+public class HeadlessPacket implements Packet {
+
+    /**
+     *
+     */
+    private final InputStream payload;
+
+    /**
+     *
+     * @param payload
+     */
+    public HeadlessPacket(InputStream payload) {
+        this.payload = payload;
+    }
+
+    @Override
+    public PacketIdentifier getIdentifier() {
+        return null;
+    }
+
+    @Override
+    public InputStream getPayload() {
+        return this.payload;
+    }
+
 }
